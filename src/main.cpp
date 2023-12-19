0c3a89b6d33be59def8ea5632edde9959b3c5cd0
pseudograph <sauravvm237@gmail.com>
2023-12-19 20:05:05 +0800

    pre-remove repos and resize from bg

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 
 #include "entities/background/Background.h"
+#include "entities/text/TextBlock.h"
 #include "virgil/Virgil.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     Virgil virgil;
     Screen screen{"image"};
-    const std::shared_ptr<Entity> background = std::make_shared<Background>("19201080bg.png", 0.0, 0.0, 0.5, 1.0, WHITE);
+    const std::shared_ptr<Entity> background = std::make_shared<Background>("19201080bg.png", 0.0, 0.0, 1.0, 1.0, WHITE);
     screen.setLayerCount(1);
     screen.insertEntity(0, background);
+    const std::shared_ptr<Entity> textBlock = std::make_shared<TextBlock>(0, 0, 400, 200, "test", GetFontDefault(), RED, 120, 1, true);
+    screen.insertEntity(0, textBlock);
     virgil.viewport.insertScreen(screen);
     virgil.run();
     return 0;
