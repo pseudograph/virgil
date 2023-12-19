c1a3cc92bc24b55fb13fe7828d6ddf7219a3e0b8
pseudograph <sauravvm237@gmail.com>
2023-12-19 19:58:47 +0800

    fix Rect instantiation bug (not enough arguments)

--- src/viewport/Viewport.cpp
+++ src/viewport/Viewport.cpp
@@ -13,12 +13,12 @@ void Viewport::run() const {
     while (!WindowShouldClose()) {
         Rectangle src{0, 0, 1920, 1080};
         Rectangle dest{0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
-        RenderTexture2D target = LoadRenderTexture(1920, 1080);
+        const RenderTexture2D target = LoadRenderTexture(1920, 1080);
         if (screens.empty()) {
             fmt::println(stderr, "[Viewport:run]: No screens defined.");
         }
         BeginTextureMode(target);
-            ClearBackground(RED);
+            ClearBackground(RAYWHITE);
             for (const Screen& screen : screens) {
                 screen.draw();
             }
--- src/viewport/Viewport.h
+++ src/viewport/Viewport.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <utility>
 #include <raylib.h>
+#include <fmt/core.h>
 
 #include "config/Consts.h"
 #include "screen/Screen.h"
@@ -26,7 +27,7 @@ private:
 
 public:
     Viewport(const int width, const int height, const int fps, std::string title, const size_t activeScreen = 0) :
-             src{BASE_WIDTH, BASE_HEIGHT}, dest{static_cast<float>(width), static_cast<float>(height)},
+             src{0, 0, BASE_WIDTH, BASE_HEIGHT}, dest{0, 0, static_cast<float>(width), static_cast<float>(height)},
              fps{fps}, title{std::move(title)}, activeScreen{activeScreen} {
         Init();
     }
